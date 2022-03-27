ff43f7e75ccb59fb6bf33d5642edfe7e30f6566d
Abu <ashaikh.saad.as@gmail.com>
2022-03-27 18:44:39 +0800

    Add vehicle counter

---
+++ README.md
@@ -0,0 +1,30 @@
+## Vehicle Classification and Counting
+
+This system is able to detect and classify vehicles in 4 different classes namely *car*, *motorcycle*, *bus* and *truck* and count the total number of vehicles detected in each of those classes.
+
+
+
+### Requirements
+
+Python 3.8 or later with all [requirements.txt](requirements.txt) dependencies installed, including torch>=1.7 . To install run:
+
+$ pip install -r requirements.txt
+
+### Inference
+
+*count.py*: Detects and counts the number of vehicles detected in each frame and displays it on the image window. (suitable for images)   
+
+```bash
+$ python count.py --source data/vehicle_test_images/* 
+```
+
+*track.py*: Detects and tracks each vehicle present in the video frame providing it with a unique id number for tracking. (suitable for videos)
+ 
+ ```bash
+$ python track.py --source data/vehicle_test_videos/live.mp4
+```
+
+## Results
+### count.py
+
+![1](https://user-images.githubusercontent.com/68045710/111742863-bf097380-88c3-11eb-9003-910896bf82ce.jpg)
---
+++ track.py
@@ -0,0 +1,333 @@
+import sys
+sys.path.insert(0, './yolov5')
+
+from utils.datasets import LoadImages, LoadStreams
+from utils.general import check_img_size, non_max_suppression, scale_coords
+from utils.torch_utils import select_device, time_synchronized
+from deepsort.utils.parser import get_config
+from deepsort.deep_sort import DeepSort
+import argparse
+import os
+import platform
+import shutil
+import time
+from pathlib import Path
+import cv2
+import torch
+import torch.backends.cudnn as cudnn
+
+
+import numpy as np
+polygons = [1]
+polygons[0] = np.array([[1,200],[703,200],[703,225],[1,225]])
+all_objects = []
+car=0
+motorcycle=0
+bus=0
+truck=0
+
+
+# def refresh_stats(img):
+#     global car, motorcycle, bus, truck
+#     font = cv2.FONT_HERSHEY_PLAIN
+#     line = cv2.LINE_AA
+#     stat_text = 'cars: {}, motorbikes: {}, trucks: {}, bus: {}'.format(car, motorcycle, bus, truck)
+#     # t_size = cv2.getTextSize(stat_text, font, 2, 3)[0]
+#     cv2.rectangle(img, (5,25), (700,55), (255,255,255), cv2.FILLED)
+#     cv2.putText(img, stat_text, (11, 50), font, 2.0, (0, 0, 0), 2, line)
+#     return img
+
+def refresh_stats(img):
+    global car, motorcycle, bus, truck
+    font = cv2.FONT_HERSHEY_PLAIN
+    line = cv2.LINE_AA
+    car_stats = 'car: {}'.format(car)
+    motorbike_stats= 'motorcycle: {}'.format(motorcycle)
+    bus_stats= 'bus: {}'.format(bus)
+    truck_stats = 'truck: {}'.format(truck)
+    cv2.rectangle(img, (5,25), (260,145), (255,255,255), cv2.FILLED)
+    cv2.putText(img, car_stats, (11, 50), font, 2.0, (0, 0, 0), 2, line)
+    cv2.putText(img, motorbike_stats, (11, 80), font, 2.0, (0, 0, 0), 2, line)
+    cv2.putText(img, bus_stats, (11, 110), font, 2.0, (0, 0, 0), 2, line)
+    cv2.putText(img, truck_stats, (11, 140), font, 2.0, (0, 0, 0), 2, line)
+    return img
+
+
+palette = (2 ** 11 - 1, 2 ** 15 - 1, 2 ** 20 - 1)
+
+
+def bbox_rel(*xyxy):
+    """" Calculates the relative bounding box from absolute pixel values. """
+    bbox_left = min([xyxy[0].item(), xyxy[2].item()])
+    bbox_top = min([xyxy[1].item(), xyxy[3].item()])
+    bbox_w = abs(xyxy[0].item() - xyxy[2].item())
+    bbox_h = abs(xyxy[1].item() - xyxy[3].item())
+    x_c = (bbox_left + bbox_w / 2)
+    y_c = (bbox_top + bbox_h / 2)
+    w = bbox_w
+    h = bbox_h
+    return x_c, y_c, w, h
+
+
+def compute_color_for_labels(label):
+    """
+    Simple function that adds fixed color depending on the class
+    """
+    color = [int((p * (label ** 2 - label + 1)) % 255) for p in palette]
+    return tuple(color)
+
+
+def draw_boxes(xcl, ycl, conf, img, name, bbox, identities=None, offset=(0, 0)):
+    global all_objects
+    global car,motorcycle,bus,truck
+    for i, (box, xc, yc) in enumerate(zip(bbox, xcl, ycl)):
+        x1, y1, x2, y2 = [int(i) for i in box]
+        x1 += offset[0]
+        x2 += offset[0]
+        y1 += offset[1]
+        y2 += offset[1]
+        # box text and bar
+        id = int(identities[i]) if identities is not None else 0
+        color = compute_color_for_labels(id)
+        label = '{}{:d}'.format("", id)
+        print("")
+        #print("For ID:", label, "The vehicle is:", name, "The center coordinate are (%d, %d)" % (xc, yc))
+        
+        
+        dict_all_objects = dict(all_objects)
+        if label not in dict_all_objects and (cv2.pointPolygonTest(polygons[0], (xc,yc), False)>0):
+            all_objects.append([label,name])
+            if name=='car':
+                car+= 1
+            if name=='motorcycle':
+                motorcycle+=1
+            if name=='bus':
+                bus+= 1
+            if name=='truck':
+                truck+= 1
+
+        # displaying bounding boxes & names
+        cv2.rectangle(img, (x1, y1), (x2, y2), color, 3)
+        #t_size = cv2.getTextSize(name, cv2.FONT_HERSHEY_PLAIN, 2, 2)[0]  # get size of name
+        #t_size = cv2.getTextSize(name + "-" + label, cv2.FONT_HERSHEY_PLAIN, 2, 2)[0]  # get size of name+labels
+        # cv2.rectangle(
+        #     img, (x1, y1), (x1 + t_size[0] + 3, y1 + t_size[1] + 4), color, -1)
+        # cv2.putText(img, name, (x1, y1 +
+        #                          t_size[1] + 4), cv2.FONT_HERSHEY_PLAIN, 2, [255, 255, 255], 2)
+
+        cv2.circle(img, (xc, yc), 4, color, -1)
+    return img
+
+
+def detect(opt, save_img=False):
+    out, source, weights, view_img, save_txt, imgsz = \
+        opt.output, opt.source, opt.weights, opt.view_img, opt.save_txt, opt.img_size
+    webcam = source == '0' or source.startswith(
+        'rtsp') or source.startswith('http') or source.endswith('.txt')
+
+    # initialize deepsort
+    cfg = get_config()
+    cfg.merge_from_file(opt.config_deepsort)
+    deepsort = DeepSort(cfg.DEEPSORT.REID_CKPT,
+                        max_dist=cfg.DEEPSORT.MAX_DIST, min_confidence=cfg.DEEPSORT.MIN_CONFIDENCE,
+                        nms_max_overlap=cfg.DEEPSORT.NMS_MAX_OVERLAP, max_iou_distance=cfg.DEEPSORT.MAX_IOU_DISTANCE,
+                        max_age=cfg.DEEPSORT.MAX_AGE, n_init=cfg.DEEPSORT.N_INIT, nn_budget=cfg.DEEPSORT.NN_BUDGET,
+                        use_cuda=True)
+
+    # Initialize
+    device = select_device(opt.device)
+    if os.path.exists(out):
+        shutil.rmtree(out)  # delete output folder
+    os.makedirs(out)  # make new output folder
+    half = device.type != 'cpu'  # half precision only supported on CUDA
+
+    # Load model
+    model = torch.load(weights, map_location=device)[
+        'model'].float()  # load to FP32
+    model.to(device).eval()
+    if half:
+        model.half()  # to FP16
+
+    # Set Dataloader
+    vid_path, vid_writer = None, None
+    if webcam:
+        view_img = True
+        cudnn.benchmark = True  # set True to speed up constant image size inference
+        dataset = LoadStreams(source, img_size=imgsz)
+    else:
+        view_img = True
+        save_img = True
+        dataset = LoadImages(source, img_size=imgsz)
+
+    # Get names and colors
+    names = model.module.names if hasattr(model, 'module') else model.names
+
+    # Run inference
+    t0 = time.time()
+    img = torch.zeros((1, 3, imgsz, imgsz), device=device)  # init img
+    # run once
+    _ = model(img.half() if half else img) if device.type != 'cpu' else None
+
+    save_path = str(Path(out))
+    txt_path = str(Path(out)) + '/results.txt'
+
+    for frame_idx, (path, img, im0s, vid_cap) in enumerate(dataset):
+        img = torch.from_numpy(img).to(device)
+        img = img.half() if half else img.float()  # uint8 to fp16/32
+        img /= 255.0  # 0 - 255 to 0.0 - 1.0
+        if img.ndimension() == 3:
+            img = img.unsqueeze(0)
+
+        # Inference
+        t1 = time_synchronized()
+        pred = model(img, augment=opt.augment)[0]
+
+        # Apply NMS
+        pred = non_max_suppression(
+            pred, opt.conf_thres, opt.iou_thres, classes=opt.classes, agnostic=opt.agnostic_nms)
+        t2 = time_synchronized()
+
+        # Process detections
+        for i, det in enumerate(pred):  # detections per image
+            if webcam:  # batch_size >= 1
+                p, s, im0 = path[i], '%g: ' % i, im0s[i].copy()
+            else:
+                p, s, im0 = path, '', im0s
+
+            s += '%gx%g ' % img.shape[2:]  # print string
+            save_path = str(Path(out) / Path(p).name)
+
+            cv2.line(im0, (0, 200), (640, 200), (0, 0, 255), 3)  # line to count number of vehicles
+            # cv2.rectangle(im0, (1,200), (703,220), (255,255,255), 1) # draw polygon where counting actually happens
+            print("The numbers for cars, motorbikes, bus and truck are: %d, %d, %d, %d" % (car, motorcycle, bus, truck))
+            refresh_stats(im0)
+
+            if det is not None and len(det):
+                # Rescale boxes from img_size to im0 size
+                det[:, :4] = scale_coords(
+                    img.shape[2:], det[:, :4], im0.shape).round()
+
+                # Print results
+                for c in det[:, -1].unique():
+                    n = (det[:, -1] == c).sum()  # detections per class
+                    s += '%g %ss, ' % (n, names[int(c)])  # add to string
+
+                bbox_xywh = []
+                confs = []
+                x_cl, y_cl = [], []
+
+                # Adapt detections to deep sort input format
+                for *xyxy, conf, cls in det:
+                    x_c, y_c, bbox_w, bbox_h = bbox_rel(*xyxy)
+                    obj = [x_c, y_c, bbox_w, bbox_h]
+                    bbox_xywh.append(obj)
+                    confs.append([conf.item()])
+
+                    x_cl.append(int(x_c))
+                    y_cl.append(int(y_c))
+
+                xywhs = torch.Tensor(bbox_xywh)
+                confss = torch.Tensor(confs)
+
+                # Pass detections to deepsort
+                outputs = deepsort.update(xywhs, confss, im0)
+
+                # draw boxes for visualization
+                if len(outputs) > 0:
+                    bbox_xyxy = outputs[:, :4]
+                    identities = outputs[:, -1]
+                    draw_boxes(x_cl, y_cl, conf, im0, names[int(c)], bbox_xyxy, identities)
+
+                    #print("This is x_cl and y_cl", x_cl, y_cl)
+
+                # Write MOT compliant results to file
+                if save_txt and len(outputs) != 0:
+                    for j, output in enumerate(outputs):
+                        bbox_left = output[0]
+                        bbox_top = output[1]
+                        bbox_w = output[2]
+                        bbox_h = output[3]
+                        identity = output[-1]
+                        with open(txt_path, 'a') as f:
+                            f.write(('%g ' * 10 + '\n') % (frame_idx, identity, bbox_left,
+                                                           bbox_top, bbox_w, bbox_h, -1, -1, -1, -1))  # label format
+
+            else:
+                deepsort.increment_ages()
+
+            # Print time (inference + NMS)
+            #print('%sDone. (%.3fs)' % (s, t2 - t1))
+            print('Done. (%.3fs)' % (t2 - t1))
+
+            # Stream results
+            if view_img:
+                cv2.imshow(p, im0)
+                #refresh_stats(im0)
+                if cv2.waitKey(1) == ord('q'):  # q to quit
+                    raise StopIteration
+
+            # Save results (image with detections)
+            if save_img:
+                print('saving img!')
+                if dataset.mode == 'images':
+                    cv2.imwrite(save_path, im0)
+                else:
+                    print('saving video!')
+                    if vid_path != save_path:  # new video
+                        vid_path = save_path
+                        if isinstance(vid_writer, cv2.VideoWriter):
+                            vid_writer.release()  # release previous video writer
+
+                        fps = vid_cap.get(cv2.CAP_PROP_FPS)
+                        w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+                        h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+                        vid_writer = cv2.VideoWriter(
+                            save_path, cv2.VideoWriter_fourcc(*opt.fourcc), fps, (w, h))
+                    vid_writer.write(im0)
+
+    if save_txt or save_img:
+        print('Results saved to %s' % os.getcwd() + os.sep + out)
+        if platform == 'darwin':  # MacOS
+            os.system('open ' + save_path)
+
+    print('Done. (%.3fs)' % (time.time() - t0))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--weights', type=str,
+                        default='yolov5x.pt', help='model.pt path')
+    # file/folder, 0 for webcam
+    parser.add_argument('--source', type=str,
+                        default='inference/images', help='source')
+    parser.add_argument('--output', type=str, default='runs/detect',
+                        help='output folder')  # output folder
+    parser.add_argument('--img-size', type=int, default=640,
+                        help='inference size (pixels)')
+    parser.add_argument('--conf-thres', type=float,
+                        default=0.45, help='object confidence threshold')
+    parser.add_argument('--iou-thres', type=float,
+                        default=0.5, help='IOU threshold for NMS')
+    parser.add_argument('--fourcc', type=str, default='mp4v',
+                        help='output video codec (verify ffmpeg support)')
+    parser.add_argument('--device', default='',
+                        help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--view-img', action='store_true',
+                        help='display results')
+    parser.add_argument('--save-txt', action='store_true',
+                        help='save results to *.txt')
+    # class 0 is person
+    parser.add_argument('--classes', nargs='+', type=int,
+                        default=[2, 3, 5, 7], help='filter by class')
+    parser.add_argument('--agnostic-nms', action='store_true',
+                        help='class-agnostic NMS')
+    parser.add_argument('--augment', action='store_true',
+                        help='augmented inference')
+    parser.add_argument("--config_deepsort", type=str,
+                        default="deepsort/configs/deep_sort.yaml")
+    args = parser.parse_args()
+    args.img_size = check_img_size(args.img_size)
+    print(args)
+
+    with torch.no_grad():
+        detect(args)
