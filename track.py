8464d290f6f569a73332fe62428af6ea68c3fe93
Abu <ashaikh.saad.as@gmail.com>
2022-04-27 13:37:41 +0800

    Refactor codes

--- track.py
+++ track.py
@@ -302,36 +302,22 @@ def detect(opt, save_img=False):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', type=str,
-                        default='yolov5s.pt', help='model.pt path')
+    parser.add_argument('--weights', type=str, default='yolov5s.pt', help='model.pt path')
     # file/folder, 0 for webcam
-    parser.add_argument('--source', type=str,
-                        default='data/vehicle_test_images', help='source')
-    parser.add_argument('--output', type=str, default='runs/detect',
-                        help='output folder')  # output folder
-    parser.add_argument('--img-size', type=int, default=640,
-                        help='inference size (pixels)')
-    parser.add_argument('--conf-thres', type=float,
-                        default=0.45, help='object confidence threshold')
-    parser.add_argument('--iou-thres', type=float,
-                        default=0.5, help='IOU threshold for NMS')
-    parser.add_argument('--fourcc', type=str, default='mp4v',
-                        help='output video codec (verify ffmpeg support)')
-    parser.add_argument('--device', default='',
-                        help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
-    parser.add_argument('--view-img', action='store_true',
-                        help='display results')
-    parser.add_argument('--save-txt', action='store_true',
-                        help='save results to *.txt')
+    parser.add_argument('--source', type=str, default='data/vehicle_test_images', help='source')
+    parser.add_argument('--output', type=str, default='runs/detect', help='output folder')  # output folder
+    parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
+    parser.add_argument('--conf-thres', type=float, default=0.45, help='object confidence threshold')
+    parser.add_argument('--iou-thres', type=float, default=0.5, help='IOU threshold for NMS')
+    parser.add_argument('--fourcc', type=str, default='mp4v', help='output video codec (verify ffmpeg support)')
+    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--view-img', action='store_true', help='display results')
+    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
     # class 0 is person
-    parser.add_argument('--classes', nargs='+', type=int,
-                        default=[2, 3, 5, 7], help='filter by class')
-    parser.add_argument('--agnostic-nms', action='store_true',
-                        help='class-agnostic NMS')
-    parser.add_argument('--augment', action='store_true',
-                        help='augmented inference')
-    parser.add_argument("--config_deepsort", type=str,
-                        default="deepsort/configs/deep_sort.yaml")
+    parser.add_argument('--classes', nargs='+', type=int, default=[0, 1, 2, 3], help='filter by class') # default=[2, 3, 5, 7]
+    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
+    parser.add_argument('--augment', action='store_true', help='augmented inference')
+    parser.add_argument("--config_deepsort", type=str, default="deepsort/configs/deep_sort.yaml")
     args = parser.parse_args()
     args.img_size = check_img_size(args.img_size)
     print(args)
