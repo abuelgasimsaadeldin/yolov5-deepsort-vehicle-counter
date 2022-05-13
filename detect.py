123bb36003cf7c78ace63c7ff5a2c7685826b665
Abu <ashaikh.saad.as@gmail.com>
2022-05-13 14:32:04 +0800

    Update parser arguments

--- detect.py
+++ detect.py
@@ -158,14 +158,13 @@ if __name__ == '__main__':
     parser.add_argument('--weights', nargs='+', type=str, default='vehicles.pt', help='model.pt path(s)')
     parser.add_argument('--source', type=str, default='data/images', help='source')  # file/folder, 0 for webcam
     parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
-    parser.add_argument('--conf-thres', type=float, default=0.25, help='object confidence threshold')
-    parser.add_argument('--iou-thres', type=float, default=0.45, help='IOU threshold for NMS')
+    parser.add_argument('--conf-thres', type=float, default=0.45, help='object confidence threshold')
+    parser.add_argument('--iou-thres', type=float, default=0.55, help='IOU threshold for NMS')
     parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
     parser.add_argument('--view-img', default=True, action='store_true', help='display results')
     parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
     parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
-    parser.add_argument('--classes', default=[0, 1, 2, 3], nargs='+', type=int,  # default=[2, 3, 5, 7]
-                        help='filter by class: --class 0, or --class 0 2 3')
+    parser.add_argument('--classes', default=[0, 1, 2, 3], nargs='+', type=int, help='coco vehicle classes = 2,3,5,7')
     parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
     parser.add_argument('--augment', action='store_true', help='augmented inference')
     parser.add_argument('--update', action='store_true', help='update all models')
--- track.py
+++ track.py
@@ -287,18 +287,16 @@ def detect(opt, save_img=False):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--weights', type=str, default='vehicles.pt', help='model.pt path')
-    # file/folder, 0 for webcam
-    parser.add_argument('--source', type=str, default='data/vehicle_test_images', help='source')
-    parser.add_argument('--output', type=str, default='runs/result', help='output folder')  # output folder
+    parser.add_argument('--source', type=str, default='data/vehicle_test_videos/live.mp4', help='source')
+    parser.add_argument('--output', type=str, default='runs/result', help='output folder')
     parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
     parser.add_argument('--conf-thres', type=float, default=0.45, help='object confidence threshold')
-    parser.add_argument('--iou-thres', type=float, default=0.5, help='IOU threshold for NMS')
+    parser.add_argument('--iou-thres', type=float, default=0.55, help='IOU threshold for NMS')
     parser.add_argument('--fourcc', type=str, default='mp4v', help='output video codec (verify ffmpeg support)')
     parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
     parser.add_argument('--view-img', action='store_true', help='display results')
     parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
-    # class 0 is person
-    parser.add_argument('--classes', nargs='+', type=int, default=[0, 1, 2, 3], help='filter by class') # default=[2, 3, 5, 7]
+    parser.add_argument('--classes', nargs='+', type=int, default=[0, 1, 2, 3], help='coco vehicle classes = 2,3,5,7')
     parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
     parser.add_argument('--augment', action='store_true', help='augmented inference')
     parser.add_argument("--config_deepsort", type=str, default="deepsort/configs/deep_sort.yaml")
